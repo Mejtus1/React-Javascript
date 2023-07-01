dec0474d46e2b0fc790d44b4b62d9979bfc74cef
Mejtus1 <GitMaster1769@proton.me>
2023-07-01 21:32:23 +0200

    modules

--- metaJavaScriptTutorial2.js
+++ metaJavaScriptTutorial2.js
@@ -1,1 +1,25 @@
-//
+//Javascript Modules
+// are standalone units of code that you can reuse again and again 
+/* Stand alone modules
+- Add modules to programs
+- Remove modules
+- Replace modules 
+
+metaJavaScriptTutorial3.js = old JS modules
+(problems: global functions - problems with overwriting )
+metaJavaScriptTutorial4.js = New ES6 JS syntax 
+metaJavaScriptTutorial3.js */
+
+export const informalGreeting = (person) => {
+    console.log("Hello, ${person}");
+}
+
+export const formalGreeting = (person) => {
+    console.log("Good day, ${person}")
+}   
+
+const greeting = () => { 
+    console.log("Howdy!");
+}
+
+export default greeting;
