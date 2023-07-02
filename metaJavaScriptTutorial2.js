51af409f1c83d0c9d6b2e3427ac32842486f0f30
Mejtus1 <GitMaster1769@proton.me>
2023-07-02 11:01:37 +0200

    DOM manipulation

--- metaJavaScriptTutorial2.js
+++ metaJavaScriptTutorial2.js
@@ -23,3 +23,32 @@ const greeting = () => {
 }
 
 export default greeting;
+
+
+
+
+//.........................................................................................................//
+//.........................................................................................................//
+//Javascript DOM manipulation 
+//DOM = document object model 
+//DOM tree structure - HTML file is mapped out like a tree structure
+//with nested objects for different partos of the page
+
+//editing a local DOM does not affect the document on a webpage 
+//using DOM allows you to manipulate live websites 
+
+//we want to add an h2 element to the DOM
+//this is written in web browser, using developer tools and console 
+const h2 = document.createElement("h2")
+//undefined
+h2.innerText = "This is a h2 heading"
+//This is a h2 heading 
+h2.setAttribute("id", "sub-heading")
+//undefined
+h2.setAttribute("class", "secondary")
+//undefined
+h2
+//<h2 id="sub-heading" class="secondary">This is an h2 heading</h2>
+document.body.appendChild(h2)
+//<h2 id="sub-heading" class="secondary">This is an h2 heading</h2>
+//THESE ARE examples of DOM manipulation and its basic methods 
